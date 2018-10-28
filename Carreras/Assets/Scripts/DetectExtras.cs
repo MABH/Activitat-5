39f533a953a1517143fbe938b710f81c85db0cbf
Sobremesa\MA <mmaabbhh@yahoo.es>
2018-10-28 23:14:39 +0100

    Commit

---
+++ Carreras/Assets/Scripts/DetectExtras.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectExtras : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+         if (col.tag =="Extra" && gameObject.tag=="Player")
+         {
+             GameControl.timeRemaining = GameControl.timeRemaining + 10;
+             Destroy(col.gameObject);
+             Debug.Log("ExtraTime");
+         }
+         else if (col.tag == "Middle" && gameObject.tag == "Player")
+         {
+             GameControl.wayPointMiddle = true;
+             Debug.Log("MiddlePoint");
+         }
+         // Otherwise if the player manages to shoot himself...
+         else if (col.tag == "Meta" && gameObject.tag == "Player")
+         {
+             GameControl.wayPointMeta = true;
+             Debug.Log("MetaPoint");
+         }
+
+        /*if (col.tag == "Player" && gameObject.tag == "Extra")
+        {
+            GameControl.timeRemaining = GameControl.timeRemaining + 10;
+            Destroy(col.gameObject);
+            Debug.Log("ExtraTime");
+        }
+        else if (col.tag == "Player" && gameObject.tag == "Middle")
+        {
+            GameControl.wayPointMiddle = true;
+            Debug.Log("MiddlePoint");
+        }
+        // Otherwise if the player manages to shoot himself...
+        else if (col.tag == "Player" && gameObject.tag == "Meta")
+        {
+            GameControl.wayPointMeta = true;
+            Debug.Log("MetaPoint");
+        }*/
+    }
+}
---
+++ Carreras/Assets/Scripts/GameControl.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    public GameObject[] rigidBody;
+    public Text buttonText,reloj;
+    public GameObject startButton;
+    public static int timeRemaining = 3;
+    bool itsGo = false;
+    public static bool wayPointMeta, wayPointMiddle;
+    string texto;
+
+    // Use this for initialization
+    void Start()
+    {
+        wayPointMeta = false;
+        wayPointMiddle = false;
+        texto = "";
+        rigidBody[0].GetComponent<Rigidbody>().isKinematic = true;
+        rigidBody[1].GetComponent<Rigidbody>().isKinematic = true;
+        rigidBody[2].GetComponent<Rigidbody>().isKinematic = true;
+        rigidBody[3].GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wayPointMeta && !wayPointMiddle)
+        {
+            texto = "Has hecho trampas";
+            timeRemaining = -10;
+            reloj.text = texto;
+        }
+        else if (wayPointMeta && wayPointMiddle)
+        {
+            texto = "Has llegado a la meta¡¡";
+            timeRemaining = -10;
+            reloj.text = texto;
+        }
+       
+    }
+
+        public void StartGame()
+    {
+        startButton.GetComponent<Button>().enabled = false;
+        StartCoroutine(Temporizador(buttonText));
+        Time.timeScale = 1;
+    }
+
+    IEnumerator Temporizador(Text textToShow)
+    {
+        while (timeRemaining>=0)
+        {
+            yield return new WaitForSeconds(1);
+            textToShow.text = texto+timeRemaining.ToString();
+            timeRemaining--;
+        }
+        if(!itsGo)
+            Go();
+        if (timeRemaining <= 0 && !wayPointMeta)
+        {
+            textToShow.text = "Has perdido";
+        }
+    }
+
+    void Go()
+    {
+        texto = "Time: ";
+        itsGo = true;
+        rigidBody[0].GetComponent<Rigidbody>().isKinematic = false;
+        rigidBody[1].GetComponent<Rigidbody>().isKinematic = false;
+        rigidBody[2].GetComponent<Rigidbody>().isKinematic = false;
+        rigidBody[3].GetComponent<Rigidbody>().isKinematic = false;
+        startButton.SetActive(false);
+        timeRemaining = 60;
+        StartCoroutine(Temporizador(reloj));
+    }
+}
